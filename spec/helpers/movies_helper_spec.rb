1b2302c5cfe6197c4363f68d889453edc4233361
Jurgis Lukss <jurgis@Jurgis-PC.(none)>
2012-08-25 19:07:26 +0300

    fixed helper test

--- spec/helpers/movies_helper_spec.rb
+++ spec/helpers/movies_helper_spec.rb
@@ -3,10 +3,10 @@ require "spec_helper"
 describe MoviesHelper do
     describe "#oddness" do
         it "should return true if the number is odd" do
-            helper.oddness(2).should be_true
+            helper.oddness(2).should == "even"
         end
         it "should return false if the number is not odd" do
-            helper.oddness(5).should be_false
+            helper.oddness(5).should == "odd"
         end
     end
 end
